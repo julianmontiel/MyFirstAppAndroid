0e32341c7f472db88ef2159e3b0c7b370d216982
Julian Montiel <julian.montiel@grobanking.com>
2016-02-27 19:02:25 -0600

    Third commit

--- app/src/main/java/com/example/julian/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/julian/myfirstapp/DisplayMessageActivity.java
@@ -33,7 +33,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(message + " ES UN SHOTITO NALGA FACIL2");
+        textView.setText(message + " ES UN SHOTITO NALGA FACIL3");
 
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
